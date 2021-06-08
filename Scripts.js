d549d7b008c2444b2505cf715777b30046864352
NasarKamish <Nasarkamish@gmail.com>
2021-06-08 08:48:49 +0200

    Fist commit

---
+++ Scripts.js
@@ -0,0 +1,36 @@
+let itemlist = document.getElementById("itemList");
+
+let form = document.getElementById("subDiv");
+
+form.addEventListener("submit", additem);
+
+let items = document.getElementsByClassName("X");
+
+items.addEventListener("click", removeItem);
+
+function additem(e) {
+  e.preventDefault();
+  let addInput = document.getElementById("addInput").value;
+  let li = document.createElement("li");
+  li.className = "item";
+  li.appendChild(document.createTextNode(addInput));
+  console.log(li);
+
+  let BTN = document.createElement("button");
+  BTN.className = "X delete";
+  BTN.appendChild(document.createTextNode("X"));
+
+  li.appendChild(BTN);
+
+  itemlist.appendChild(li);
+}
+
+function removeItem(e) {
+  //   if (e.target.classlist.contains("delete")) {
+  // //     if (confirm("Are you sure?")) {
+  // //       let li = e.target.parentElement;
+  // //       itemlist.removeChild(li);
+  // //     }
+  // //   }
+  console.log(1);
+}
